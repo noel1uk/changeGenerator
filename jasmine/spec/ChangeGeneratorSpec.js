2497832c1fa1d0f7feae4ed3cf93776299b528e3
Noel Vock <noel1uk@gmail.com>
2018-05-14 17:06:28 +0100

    test: ensure turnToNumber returns 80 when given 80p

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -9,14 +9,17 @@ describe('ChangeGenerator', function() {
       })
 
       it('returns `1 x 20p`', function() {
-        expect(cg.change).toEqual('1 x 20p');
+        expect(cg.change()).toEqual('1 x 20p');
       });
 
       describe('.turnToNumber', function() {
-        // let cg = new ChangeGenerator('£1', '80p');
     
         it('returns the number 100 when give the string £1', function() {
           expect(cg.turnToNumber('£1')).toEqual(100);
+        });
+        
+        it('returns the number 80 when given the string 80p', function() {
+          expect(cg.turnToNumber('80p')).toEqual(80);
         });
       });
     });
