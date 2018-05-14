deed0ffbf312a6439d24dac273c911f46ebde2da
Noel Vock <noel1uk@gmail.com>
2018-05-14 16:31:02 +0100

    test: ensure `turnToNumber` returns 100 when given £1

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -1,13 +1,19 @@
 describe('ChangeGenerator', function() {
+  let cg = new ChangeGenerator('£1', '80p');
   
   describe('.change', function() {
     
     describe('when given £1', function() {
-      let cg = new ChangeGenerator('£1', '80p');
 
       it('returns `1 x 20p`', function() {
         expect(cg.change).toEqual('1 x 20p');
       });
     });
   })
+
+  describe('.turnToNumber', function() {
+    it('returns the number 100 when give the string 100', function() {
+      expect(cg.turnToNumber('£1')).toEqual(100);
+    });
+  });
 });
