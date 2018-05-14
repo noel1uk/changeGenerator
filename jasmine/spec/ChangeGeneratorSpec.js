f7ac5c78fa7b18711e95727cc57301475d747193
Noel Vock <noel1uk@gmail.com>
2018-05-14 16:52:05 +0100

    refactor: fix error and add beforeEach

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -1,19 +1,25 @@
 describe('ChangeGenerator', function() {
-  let cg = new ChangeGenerator('£1', '80p');
   
   describe('.change', function() {
     
     describe('when given £1', function() {
 
+      beforeEach(function() {
+        cg = new ChangeGenerator('£1', '80p');
+      })
+
       it('returns `1 x 20p`', function() {
         expect(cg.change).toEqual('1 x 20p');
       });
+
+      describe('.turnToNumber', function() {
+        // let cg = new ChangeGenerator('£1', '80p');
+    
+        it('returns the number 100 when give the string £1', function() {
+          expect(cg.turnToNumber('£1')).toEqual(100);
+        });
+      });
     });
   })
-
-  describe('.turnToNumber', function() {
-    it('returns the number 100 when give the string 100', function() {
-      expect(cg.turnToNumber('£1')).toEqual(100);
-    });
-  });
+  
 });
